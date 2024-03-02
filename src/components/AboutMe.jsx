6305c53d7737d66d7623df91ba03735ab9846f0e
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-03-02 21:23:08 +0000

    profile added

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutMe=()=>{
+
+return(
+    <div className="md:7/12 lg:w-6/12">
+        <h2 className="text-2xl  font-bold md:text-4xl">About Me</h2>
+        <p className="mt-6 ">Placing user need at the heart of everything that I do, 
+        I have applied Agile, Scrum and Lean Principles to help teams deliver project,
+         products and goals on time, to scope and within budget. 
+         Now I have found a new passion to build products myself! 
+         I am finding out how much I love building things. This is fun. Enjoying the Bootcamp!</p>
+     </div>
+
+);
+
+
+}
+
+export default AboutMe;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar=()=>{
  return (<div>
     <div class="flex space-x-4">
-        <a href="#profile" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About me</a>
+        <a href="#aboutme" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About me</a>
         <a href="#contact" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact me</a>
         <a href="#projects"className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">My Projects</a>
        
