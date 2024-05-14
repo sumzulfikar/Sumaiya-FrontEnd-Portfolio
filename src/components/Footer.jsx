819331ad2290de0ae1d6f5877c1ae7b25b020415
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-05-14 17:20:49 +0100

    adding g tag and minor updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const year=new Date();
-const Footer=()=>{
+const year = new Date();
+const Footer = () => {
 
-return <h6 className="text-center font-normal">{year.getFullYear()} © All Rights Reserved - Sumaiya Zulfikar</h6>
+    return <h6 className="text-center font-normal">{year.getFullYear()} © Sumaiya Zulfikar. All Rights Reserved.</h6>
 
 }
 export default Footer;
