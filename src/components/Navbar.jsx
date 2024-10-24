a033ebd8315d085c9d6a33d8c668f61ac8a3ed91
sumzul <sum@myfundmobile.com>
2024-10-24 21:14:47 +0100

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
 
             <Link to="/" className="bg-gray-900 text-stone-50 rounded-md px-3 py-2 text-sm sm:text-xs font-medium " aria-current="page" >Home</Link>
 
-            <Link to="/aboutme" className="text-gray-300 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">About me</Link>
+            <Link to="/aboutme" className="text-gray-800 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">About me</Link>
 
-            <Link to="/myskills" className="text-gray-300 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">Tech Stack</Link>
+            <Link to="/myskills" className="text-gray-800 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">Tech Stack</Link>
 
-            <Link to="/myprojects" className="text-gray-300 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">Projects</Link>
+            <Link to="/myprojects" className="text-gray-800 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">Projects</Link>
 
             {/* <Link to="/myresume" className="text-gray-300 hover:bg-gray-700 hover:text-stone-50 rounded-md px-3 py-2 text-sm font-medium">Resume</Link> 
 
