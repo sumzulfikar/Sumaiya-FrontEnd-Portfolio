253463056e7002f8df14fbea87db1a186b20d525
Sumaiya Zulfikar <150956638+sumzulfikar@users.noreply.github.com>
2024-10-25 04:29:57 +0100

    Update RuledPostit.jsx

--- src/components/projects/RuledPostit.jsx
+++ src/components/projects/RuledPostit.jsx
@@ -6,7 +6,7 @@ const RulesPostitCard = ({ userstory }) => {
     return (
         <>
 
-            <div className="rounded-lg shadow-md bg-white p-4 flex flex-col gap-2">
+            <div className="rounded-lg shadow-md bg-white pt-2 flex flex-col gap-2">
 
                 <p className="text-base text-gray-700">{userstory}</p>
 
